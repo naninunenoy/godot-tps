397cf0cb40e12cf514cbf73cd7abbef506703326
naninunenoy <naninuneno0411@gmail.com>
2026-04-29 19:40:25 +0900

    fix editorconfig

--- Main.cs
+++ Main.cs
@@ -1,18 +1,15 @@
-using Godot;
 using System;
-
+using Godot;
 namespace tps;
 
 public partial class Main : Node3D
-{
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("main ready");
-	}
+{   // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print("main ready");
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    { }
 }
