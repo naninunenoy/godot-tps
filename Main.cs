991f18cf70475c8b16beea71ed32cea8469ccf21
naninunenoy <naninuneno0411@gmail.com>
2026-04-29 19:30:44 +0900

    add script

---
+++ Main.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+namespace tps;
+
+public partial class Main : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("main ready");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
